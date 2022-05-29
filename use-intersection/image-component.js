7a98c9f796b94387ec3bafaedcb97c6a8e927553
Dieu Anh Minh <minh@wearebloom.com.au>
2022-05-29 16:07:47 +0700

    first commit

---
+++ use-intersection/image-component.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "./image";
+import useIntersectionObserver from "./use-intersection";
+
+const ImageConponent = props => {
+  const ref = React.useRef();
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  useIntersectionObserver({
+    target: ref,
+    onIntersect: ([{ isIntersecting }], observerElement) => {
+      if (isIntersecting) {
+        if (!isVisible) {
+          props.onIsVisible();
+          setIsVisible(true);
+        }
+        observerElement.unobserve(ref.current);
+      }
+    }
+  });
+
+  const aspectRatio = (props.height / props.width) * 100;
+
+  return (
+    <a
+      href={props.url}
+      ref={ref}
+      rel="noopener noreferrer"
+      target="_BLANK"
+      className="image-container"
+      style={{ paddingBottom: `${aspectRatio}%` }}
+    >
+      {isVisible && (
+         <Image src={props.src} thumb={props.thumb} alt={props.alt} />
+      )}
+    </a>
+  );
+};
+
+export default ImageConponent;
